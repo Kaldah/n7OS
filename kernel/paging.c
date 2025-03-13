79a817ab9e378c2608604c81ca28c68cc6c82e8c
Corentin C <corentin.cousty@gmail.com>
2025-03-13 09:47:37 +0100

    Update paging.c

--- kernel/paging.c
+++ kernel/paging.c
@@ -1,12 +1,15 @@
 #include <n7OS/paging.h>
 #include <stddef.h> // nécessaire pour NULL
 
-void initialise_paging() {
-    // On initialise la table de pages : CR3
-    uint32_t page_directory_adresse_CR3 = kmalloc_a(sizeof(PDE) * 1024);
-    PageDirectory page_directory = (PDE *) page_directory_adresse_CR3;
+PageDirectory page_directory;
 
-    printf("Page Directory adresse (CR3): %x\n", page_directory_adresse_CR3);
+void initialise_paging() {
+    // On initialise le répertoire de pages
+    PageDirectory page_directory = kmalloc_a(sizeof(PDE) * 1024);
+    // On nettoie la mémoire du répertoire de pages
+    memset(page_directory, 0, sizeof(PDE) * 1024);
+
+    printf("Page Directory adresse : %x\n", page_directory);
     
     // On initialise les tables de pages
     for (int i = 0; i < 1024; i++) {
@@ -16,13 +19,37 @@ void initialise_paging() {
         // On initialise la table de page
         PageTable page_table = (PTE *) page_table_adresse;
 
+        // On obtient l'adresse dont les 12 bits de poids faibles sont à 0
+        int32_t page_adress = findfreePage();
+
+        page_table_entry_t page_table_entry;
+        // On marque l'adresse de la page dans la mémoire physique
+        page_table_entry.page = page_adress >> 12;
+        // On marque la page comme accessible en mode noyau
+        page_table_entry.U = 0;
+        // On marque la page comme accessible en écriture
+        page_table_entry.W = 1;
+        // On marque la page comme présente en mémoire
+        page_table_entry.P = 1;
+
         // On initialise la table de page avec des pages libres
         for (int j = 0; j < 1024; j++) {
             // On initialise l'entrée de la table de page
             page_table_entry_t page_table_entry;
 
-            // On obtient l'adresse dont les 12 bits de poids faibles sont à 0
-            int32_t page_adress = findfreePage();
+            // On marque l'adresse de la page dans la mémoire physique
+            page_table_entry.page = page_adress >> 12;
+            // Dirty Bit, page non modifiée lors de l'initialisation
+            page_table_entry.D = 0;
+            // Accessed bit, la page n'a pas été accédée lors de l'initialisation
+            page_table_entry.A = 0;
+            // On marque la page comme accessible en mode noyau
+            page_table_entry.U = 0;
+            // On marque la page comme accessible en écriture
+            page_table_entry.W = 1;
+            // On marque la page comme présente en mémoire
+            page_table_entry.P = 1;
+
             page_table[j] = (PTE) page_table_entry ;
         }
 
