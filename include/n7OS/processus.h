66a5bb919eee24da0b50d9dc845d6318280bd790
Corentin Cousty <45534019+Kaldah@users.noreply.github.com>
2025-05-25 00:36:06 +0200

    Update processus.h

--- include/n7OS/processus.h
+++ include/n7OS/processus.h
@@ -13,7 +13,7 @@
 #define NB_PROC 255 // Nombre maximum de processus
 #define STACK_SIZE 1024 // Taille de la pile d'un processus
 #define MAX_RESOURCES 10 // Nombre maximum de ressources par processus
-#define TIME_SLOT 1000 // Durée d'un quantum de temps (en ms)
+#define TIME_SLOT 200 // Durée d'un quantum de temps (en ms)
 
 typedef enum {ELU, PRET_ACTIF, PRET_SUSPENDU, BLOQUE_ACTIF,
     BLOQUE_SUSPENDU, TERMINE} PROCESS_STATE;
